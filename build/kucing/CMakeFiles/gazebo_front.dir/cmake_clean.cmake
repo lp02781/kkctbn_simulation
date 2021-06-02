4f411eb00218312e46c5267a4fc31b53f86ae5a7
lp02781 <lp02781@gmail.com>
2021-06-02 23:40:22 +0700

    bodo amat

---
+++ build/kucing/CMakeFiles/gazebo_front.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gazebo_front.dir/src/image/gazebo_front.cpp.o"
+  "/home/mfikih15/kkctbn_simulation/devel/lib/kucing/gazebo_front.pdb"
+  "/home/mfikih15/kkctbn_simulation/devel/lib/kucing/gazebo_front"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gazebo_front.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/kucing/CMakeFiles/gazebo_master.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gazebo_master.dir/src/main/gazebo_master.cpp.o"
+  "/home/mfikih15/kkctbn_simulation/devel/lib/kucing/gazebo_master.pdb"
+  "/home/mfikih15/kkctbn_simulation/devel/lib/kucing/gazebo_master"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gazebo_master.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
